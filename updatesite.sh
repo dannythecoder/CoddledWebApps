7536188ea258198a83eff60c474017cab9709022
Danny Caudill <dan@coddledsoftware.com>
2015-10-15 21:58:25 -0400

    Added a simple script to simplify site updates.

---
+++ updatesite.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# Update the website with the current webapps
+#
+# Usage: ./updatesite.sh </path/to/site/root>
+
+if [ "$#" != "1" ];
+then
+    echo "Usage: $0 </path/to/site/root>"
+    exit 1
+fi
+
+# Simply update this list as new directories are added.
+# $1 contains the path to the root of the website.
+
+cp -r js $1/
+cp -r relaxinghues $1/
+cp -r cloudynight $1/
+
