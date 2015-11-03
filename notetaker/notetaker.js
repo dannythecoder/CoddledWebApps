2a22df4fb46861e1463bc0ad20352a99d0615689
DannyTheCoder <dan@coddledsoftware.com>
2015-11-03 11:38:41 -0500

    Note Taker now shows last save time. Improved webapp titles.

--- notetaker/notetaker.js
+++ notetaker/notetaker.js
@@ -13,6 +13,8 @@
  */
 function startNoteTaker() {
     if (typeof(Storage) !== "undefined") {
+
+        // Retrieve Content, if available
         content = localStorage.getItem("notes");
         if (content !== null) {
             document.getElementById("notes").value = content;
@@ -20,9 +22,23 @@ function startNoteTaker() {
             document.getElementById("notes").value = "";
         }
 
+        // Retrieve last save time, if available.
+        saveText = localStorage.getItem("saveTime");
+        if (content !== null) {
+            document.getElementById("save").value = saveText;
+        } else {
+            document.getElementById("save").value = "Save";
+        }
+
     } else {
         document.getElementById("notes").value = "This ancient browser does not support local storage...";
     }
+
+    // Register resize listener
+    window.addEventListener('resize', resizeTextArea, false);
+
+    // Call resize for the first time.
+    resizeTextArea();
 }
 
 
@@ -32,9 +48,38 @@ function startNoteTaker() {
 function saveNotes() {
 
     if (typeof(Storage) !== "undefined") {
-        content =  document.getElementById("notes").value;
+        // Update the saved notes.
+        content = document.getElementById("notes").value;
         localStorage.setItem("notes", content);
+
+        // Update the save time.
+        var currTime = new Date()
+        var saveText = ("Save (last: " +
+                        currTime.getFullYear() + "/" +
+                        pad(currTime.getMonth(), 2) + "/" +
+                        pad(currTime.getDay(), 2) + " " +
+                        pad(currTime.getHours(), 2) + ":" +
+                        pad(currTime.getMinutes(), 2) + ":" +
+                        pad(currTime.getSeconds(), 2) + ")");
+        document.getElementById("save").value = saveText;
+        localStorage.setItem("saveTime", saveText);
     } else {
         document.getElementById("notes").value = "This browser does not support local storage...all is lost.";
     }
+}
+
+/**
+ * Provide zero-padding for a number.
+ */
+function pad(num, size) {
+    var s = "000000000" + num;
+    return s.substr(s.length-size);
+}
+
+/**
+ * Resize the text area to match the full height of the window.
+ */
+function resizeTextArea() {
+    var textarea = document.getElementById("notes");
+    textarea.style.height = "" + Math.floor(window.innerHeight * 0.92) + "px";
 }
